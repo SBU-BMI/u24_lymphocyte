661242b527fbc4d91161f97f7cfc0031d7b7c56e
scotthoule <lehhou@bright70.cm.cluster>
2017-08-24 10:40:17 -0400

    Update svs2heatmap.sh

--- lym_pipeline/svs2heatmap.sh
+++ lym_pipeline/svs2heatmap.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+# This script will execute for days, run it with nohup!
 
-cd camic_setup
-bash start.sh
-cd ..
+source ./conf/bashrc_base.sh
 
+# Break images under svs/ to png tiles
 cd patch_extraction
 bash start.sh
 cd ..
 
+# Apply the trained CNN to predict all patches
 cd prediction
 bash start.sh
 cd ..
 
+# Generate heatmap json files
 cd heatmap_gen
 bash start.sh
 cd ..
