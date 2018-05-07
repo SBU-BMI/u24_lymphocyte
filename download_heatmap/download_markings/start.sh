26ef48a4a30823a9c71470e83a7259fbbffb7e07
scotthoule <lehou0312@gmail.com>
2018-05-07 10:37:56 -0400

    Update start.sh

--- download_heatmap/download_markings/start.sh
+++ download_heatmap/download_markings/start.sh
@@ -16,23 +16,27 @@ bash download_markings.sh ${ctype} ${username} ${case_list} ${raw_marking_output
 # you also need the human chosen lymphocyte sensitivity,
 # necrosis specificity, and smoothness.
 # You need to get those numbers manually from caMicroscope and put
-# them in a file named ./data/${slide_id}_${username}.txt
+# them in a file named ${RAW_MARKINGS_PATH}/${slide_id}_${username}.txt
 # For example:
-#     ./data/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_weight.txt
+#     ${RAW_MARKINGS_PATH}/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_weight.txt
 # There should be three lines in the weight file, the first
 # line shows the chosen lymphocyte sensitivity, the second
 # line shows the chosen necrosis specificity, the last
 # line shows the smoothness.
 # For example:
-#     cat ./data/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_weight.txt
+#     cat ${RAW_MARKINGS_PATH}/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_weight.txt
 #     0.77
 #     0.60
 #     0.15
 
-# At the end, ./data/ folder should contain both
+# At the end, ${RAW_MARKINGS_PATH}/ folder should contain both
 # the weight files and the markup files.
 # For example:
-#    ./data/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_mark.txt
-#    ./data/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_weight.txt
+#    ${RAW_MARKINGS_PATH}/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_mark.txt
+#    ${RAW_MARKINGS_PATH}/TCGA-NJ-A55O-01Z-00-DX1_rajarsi.gupta_weight.txt
+# The *_mark.txt file has the following format:
+# SlideID  UserName  MarkLabel  MarkWidth  TimeStamp  Coordinates(alternating<x,y>)
+# For example:
+# TCGA-3C-AALI-01Z-00-DX2 rajarsi.gupta@stonybrook.edu    TumorPos        1       1501535710324   0.1362388819418,0.97854705021941,0.136239,0.978547,0.135192,0.978547,0.134145,0.978547,0.134145,0.978547,0.132351,0.978547,0.130556,0.978547,0.128762,0.978547,0.126967,0.978547,0.125173,0.978547,0.123378,0.978547,0.121584,0.978547,0.121584,0.978547,0.119781,0.978222,0.117978,0.977897,0.116175,0.977572,0.114373,0.977247,0.11257,0.976922,0.110767,0.976597,
 
 exit 0
