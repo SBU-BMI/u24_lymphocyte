4554d4beed269ba696dea705dac3ee2cd85bea65
scotthoule <lehou0312@gmail.com>
2018-05-16 17:24:42 -0400

    Update clicks_summarize.sh

--- patch_labeling_web/clicks_summarize.sh
+++ patch_labeling_web/clicks_summarize.sh
@@ -5,4 +5,29 @@ IMG=images
 ls -l clicks | grep clicked | awk -F'clicked_|.txt' '{print $2}' > clicks/list_clicked.txt
 ls -l clicks | grep ignored | awk -F'ignored_|.txt' '{print $2}' > clicks/list_ignored.txt
 
+awk '
+FILENAME=="clicks/list_ignored.txt"{
+        igr[$1]=0;
+}
+FILENAME=="clicks/list_clicked.txt"{
+        pos[$1]=0;
+}
+FILENAME=="images/info.txt"{
+        if (!($1 in igr)) {
+                if ($1 in pos)
+                        n[$NF]++
+                else
+                        n[$NF] = n[$NF] + 0
+        } else {
+                n[$NF] = n[$NF] + 0
+        }
+}
+
+END{
+        for (x in n) {
+                print x, n[x];
+        }
+}
+' clicks/list_ignored.txt clicks/list_clicked.txt images/info.txt > clicks/groups.txt
+
 exit 0
