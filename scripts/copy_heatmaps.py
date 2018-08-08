85fce022f44736c1b3faa107696058ecce5ec2c2
Han Le <32404085+hansbu@users.noreply.github.com>
2018-08-08 10:55:11 -0400

    copy_heatmaps.py
    option 1: to copy heatmaps and change their names
    option 2: copy .csv to cluster_indices/inputs

---
+++ scripts/copy_heatmaps.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+cancertype = sys.argv[1]
+out_dir = sys.argv[2]
+option = int(sys.argv[3])
+
+def copy_heatmap():
+    if option == 1:     # to copy heatmaps 
+        grayscale = '../data/grayscale_heatmaps'
+        thresholded = '../data/thresholded_heatmaps'
+
+        #rates-<cancer_type>-all-auto
+        dest = '../data/rates-{}-all-auto'.format(cancertype)
+        if not os.path.isdir(dest):
+            os.system('mkdir ' + dest)
+        else:
+            os.system('rm -rf ' + dest + '/*')
+
+        files = os.listdir(grayscale)
+        print(files)
+        for file in files:
+            if '.png' in file:
+                file_dest1 = 'rate.' + file[:-4] + '.automatic.png'
+                file_dest2 = file_dest1[:-4] + '_thres.png'
+                os.system('cp ' + os.path.join(grayscale, file) + ' ' + os.path.join(dest, file_dest1))
+                os.system('cp ' + os.path.join(thresholded, file) + ' ' + os.path.join(dest, file_dest2))
+        os.system('cp ' + os.path.join(thresholded, '*.csv') + ' ' + dest)
+    elif option == 2:
+        # create input_full.csv
+        filename = out_dir[:-6] + 'input_full.csv';
+        files = os.listdir(os.path.join(out_dir, cancertype))
+        os.system('rm -rf ' + filename)
+        for file in files:
+            if '.csv' in file:
+                with open(filename, 'a') as f:
+                    f.write('inputs/' + cancertype + '/' + file + ',' + './output/' + cancertype + '/,' + cancertype + '\n')
+
+
+
+if __name__ == "__main__":
+    print('Usage: python copy_heatmap.py [cancertype] [out_dir] [func_option] \nfunc_option: 1 for copy the heatmap, 2 for create file input_full.csv')
+    if len(sys.argv) < 3: 
+        print('missing arguments')
+    else:
+        copy_heatmap()
