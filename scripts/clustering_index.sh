a34e03b51738e972fe36a8753d92ae22dd973882
Han Le <32404085+hansbu@users.noreply.github.com>
2018-08-09 18:05:46 -0400

    add generating cluster indices

--- scripts/clustering_index.sh
+++ scripts/clustering_index.sh
@@ -33,12 +33,23 @@ cd ..
 
 cd ./csv_generation
 matlab -nodisplay -singleCompThread -r \
-"get_patch_til_group_svs_file_wrap('${CANCER_TYPE}', '${OUT_DIR}', '${IN_DIR}', ${W}, ${H}); exit;" \
+"get_patch_til_svs_file_singlefile_wrap('${CANCER_TYPE}', '${OUT_DIR}', '${IN_DIR}', ${W}, ${H}); exit;" \
 </dev/null &>${LOG_OUTPUT_FOLDER}/log.get_patch_til_group_svs_file.txt
+cd ../
 wait;
 
 cd ./scripts
 python -u copy_heatmaps.py ${CANCER_TYPE} ${OUT_DIR} 2 &>> ${LOG_OUTPUT_FOLDER}/log.copy_heatmaps.txt
+cd ../
+wait;
+
+# run the Rscript
+module load R/4.3.0
+cd ./cluster_indices
+bash run_all.sh input_full.csv 6 > ${LOG_OUTPUT_FOLDER}/log.generate_clustering_indices_from_csv_files.txt
+wait;
+
+bash collateClusterIdx.sh > ${LOG_OUTPUT_FOLDER}/log.collateClusterIndices.txt
 wait;
 
 exit 0
