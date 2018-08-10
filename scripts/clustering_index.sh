ce5a20b14ffbd16952b02bde5416bfc8ebff34b9
Han Le <32404085+hansbu@users.noreply.github.com>
2018-08-10 15:51:55 -0400

    clean up

--- scripts/clustering_index.sh
+++ scripts/clustering_index.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-cd ../conf
-source variables.sh
-cd ..
-
 CANCER_TYPE=seer
-OUT_FOL=${BASE_DIR}/cluster_indices
-OUT_DIR=${OUT_FOL}/inputs
-IN_DIR=${BASE_DIR}/data
+OUT_FOL='../cluster_indices'
+OUT_DIR='../cluster_indices/inputs'
+IN_DIR='../data'
+LOG_OUTPUT_FOLDER='../data/log'
 W=5
 H=5
-rm -rf ${OUT_DIR}/*   # deleting existing folders/files
+rm -rf ${OUT_DIR}/*
+cd ../
 
-# generate grayscale heatmap
+
+### generate grayscale heatmap
 cd ./download_heatmap/get_grayscale_heatmaps/
 bash start.sh
 cd ../../
 wait;
 
-# run thresholding grayscale heatmap
+### run thresholding grayscale heatmap
 cd ./download_heatmap/threshold_grayscale_heatmaps/
 bash start.sh
 cd ../../
 wait;
 
-# copy heatmap over other folders named "rates-<cancer_type>-all-auto" in data
-# and heatmap format is *-automatic.png
+### copy heatmap over other folders named "rates-<cancer_type>-all-auto" in data
+### and heatmap format is *-automatic.png
 cd ./scripts
 python -u copy_heatmaps.py ${CANCER_TYPE} ${OUT_DIR} 1 &> ${LOG_OUTPUT_FOLDER}/log.copy_heatmaps.txt
 wait;
-cd ..
+cd ../
 
 cd ./csv_generation
 matlab -nodisplay -singleCompThread -r \
@@ -39,14 +38,13 @@ cd ../
 wait;
 
 cd ./scripts
-python -u copy_heatmaps.py ${CANCER_TYPE} ${OUT_DIR} 2 &>> ${LOG_OUTPUT_FOLDER}/log.copy_heatmaps.txt
+python -u copy_heatmaps.py ${CANCER_TYPE} ${OUT_FOL} 2 &>> ${LOG_OUTPUT_FOLDER}/log.copy_heatmaps.txt
 cd ../
 wait;
 
 # run the Rscript
-module load R/4.3.0
 cd ./cluster_indices
-bash run_all.sh input_full.csv 3 > ${LOG_OUTPUT_FOLDER}/log.generate_clustering_indices_from_csv_files.txt
+bash run_all.sh input_full.csv 3 > ${LOG_OUTPUT_FOLDER}/log.cluster_indices_from_csv_files.txt
 wait;
 
 bash collateClusterIdx.sh > ${LOG_OUTPUT_FOLDER}/log.collateClusterIndices.txt
