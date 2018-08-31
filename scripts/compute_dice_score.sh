d22b02b84f4726aa924c514926b7b252273e67b9
Han Le <32404085+hansbu@users.noreply.github.com>
2018-08-31 09:18:03 -0400

    usage instructions

--- scripts/compute_dice_score.sh
+++ scripts/compute_dice_score.sh
@@ -1,5 +1,12 @@
 #!bin/bash
 
+# inputs: 
+  # the prediction-xxx file at high-res located at ${HEATMAP_TXT_OUTPUT_FOLDER} 
+  # the labeled map (groundtruth) from download heatmap located at ${TUMOR_HEATMAPS_PATH}
+# output: Dice-score and Accuracy printed on the terminal screen or in the logfile ${LOG_OUTPUT_FOLDER}/log.compute_dice_score.txt
+
+# requirement: cv2. Cannot use PIL Image because the labeled heatmaps are very big (~40K X 20K), PIL cannot read such big size images
+
 source ../conf/variables.sh
 
 python -u compute_dice_score.py ${HEATMAP_TXT_OUTPUT_FOLDER} ${TUMOR_HEATMAPS_PATH} &> ${LOG_OUTPUT_FOLDER}/log.compute_dice_score.txt 
