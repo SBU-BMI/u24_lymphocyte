f6b001e43240e3e05207c56650140c1514bc84d2
Han Le <32404085+hansbu@users.noreply.github.com>
2018-09-04 15:32:56 -0400

    to be applicable to work on intersected heatmaps

--- heatmap_gen/gen_all_json.sh
+++ heatmap_gen/gen_all_json.sh
@@ -2,8 +2,8 @@
 
 source ../conf/variables.sh
 
-for files in ./patch-level-merged/prediction-*; do
-    if [[ "$files" == *.low_res ]]; then
+for files in ${HEATMAP_TXT_OUTPUT_FOLDER}/prediction-*; do
+    if [[ "$files" == *.low_res* ]]; then
         python gen_json_multipleheat.py ${files} ${HEATMAP_VERSION}-low_res  ${SVS_INPUT_PATH} lym 0.5 necrosis 0.5 &>> ${LOG_OUTPUT_FOLDER}/log.gen_json_multipleheat.low_res.txt
     else
         python gen_json_multipleheat.py ${files} ${HEATMAP_VERSION}-high_res ${SVS_INPUT_PATH} lym 0.5 necrosis 0.5 &>> ${LOG_OUTPUT_FOLDER}/log.gen_json_multipleheat.txt
