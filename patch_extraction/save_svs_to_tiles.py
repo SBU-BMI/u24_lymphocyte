e2ac68f3dcbd626de34c270c2c2369347aa5a6d3
Han Le <32404085+hansbu@users.noreply.github.com>
2019-05-01 11:29:09 -0400

    Update save_svs_to_tiles.py
    compute resolution for Multiplex IHC WSIs, .tiff images

--- patch_extraction/save_svs_to_tiles.py
+++ patch_extraction/save_svs_to_tiles.py
@@ -25,6 +25,8 @@ try:
        mag = 10.0 / float(oslide.properties[openslide.PROPERTY_NAME_MPP_X]);
     elif "XResolution" in oslide.properties:
        mag = 10.0 / float(oslide.properties["XResolution"]);
+    elif 'tiff.XResolution' in oslide.properties:   # for Multiplex IHC WSIs, .tiff images
+       mag = 10.0 / float(oslide.properties["tiff.XResolution"]);
     else:
        mag = 10.0 / float(0.254); 
     pw = int(patch_size_20X * mag / 20);
