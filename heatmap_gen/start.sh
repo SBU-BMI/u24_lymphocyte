8bc94ea142b77b39ff57aad12de7ef23f7e7bd10
scotthoule <lehou0312@gmail.com>
2019-05-09 15:07:01 -0400

    Update start.sh

--- heatmap_gen/start.sh
+++ heatmap_gen/start.sh
@@ -19,6 +19,12 @@ cp ./patch-level-color/* ${HEATMAP_TXT_OUTPUT_FOLDER}/
 bash gen_all_json.sh &> ${LOG_OUTPUT_FOLDER}/log.gen_all_json.txt
 cp ./json/* ${JSON_OUTPUT_FOLDER}/
 
+for files in ./json/meta_*; do
+    if [[ "$files" != *.low_res* ]]; then
+        cp ${files} ${HEATMAP_META_OUTPUT_FOLDER}/
+    fi
+done
+
 # Put all jsons to camicroscope
 bash upload_heatmaps.sh &> ${LOG_OUTPUT_FOLDER}/log.upload_heatmaps.txt
 
